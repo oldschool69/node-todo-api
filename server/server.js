a8e7dc316122a57c8688e447ee54bb378160ed46
Flavio Oliveira <flavio.m@samsung.com>
2018-05-23 09:56:22 -0300

    Iniciando dev da rest api

--- server/server.js
+++ server/server.js
@@ -5,4 +5,28 @@ var {mongoose} = require('./db/mogoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+var app = express();
+
+//Converte um objeto para Json e envia para a aplicação cliente
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo  = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
 

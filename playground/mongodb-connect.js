d6033931a3ee13069afa2b3f601e999966de8865
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-05-22 23:16:07 -0300

    changes from home

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,32 +10,32 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     console.log('Connected to MongoDB server');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log('Unable to insert to do', err);
-    //     }
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if(err){
+            return console.log('Unable to insert to do', err);
+        }
 
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
 
-    //Insert new doc into Users (name, age, location)
+    // Insert new doc into Users (name, age, location)
 
-    // db.collection('Users').insertOne({
-    //     name: 'Flavio Oliveira',
-    //     age: 45,
-    //     location: 'Campinas'
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log('Unable to insert user', err);
-    //     }
+    db.collection('Users').insertOne({
+        name: 'Flavio Oliveira',
+        age: 45,
+        location: 'Campinas'
+    }, (err, result) => {
+        if(err){
+            return console.log('Unable to insert user', err);
+        }
 
-    //     console.log(result.ops[0]._id.getTimestamp());
-    //     //console.log(JSON.stringify(result.ops, undefined, 2));
+        console.log(result.ops[0]._id.getTimestamp());
+        //console.log(JSON.stringify(result.ops, undefined, 2));
 
-    // });
+    });
 
     db.close();
 });

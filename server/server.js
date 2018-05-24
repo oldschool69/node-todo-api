a1f1882466c835cfca201d0580aa9d6428930e06
Flavio Oliveira <flavio.m@samsung.com>
2018-05-24 17:38:47 -0300

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mogoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./../server/middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -129,6 +130,10 @@ app.post('/users', (req, res) => {
     });
 });
 
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);    
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);

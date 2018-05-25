7f860aceefd44cdbc90d8e15bda28ce3827d1761
Flavio Oliveira <flavio.m@samsung.com>
2018-05-25 15:53:48 -0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+
 
 var {mongoose} = require('./db/mogoose');
 var {Todo} = require('./models/todo');
@@ -126,6 +128,21 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);    
 });
+
+//POST /users/login {email, password}
+
+app.post('/users/login', (req, res) =>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);

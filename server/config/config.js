3c2136042932ebd08a1a540fda239304096ecda8
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-06-04 19:22:35 -0300

    Changed some Promises to async/await calls

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development' || env === 'test'){
-    var config = require('./config.json');
-    var envConfig = config[env];
+    // var config = require('./config.json');
+    // var envConfig = config[env];
 
-    Object.keys(envConfig).forEach((key) => {
-        process.env[key] = envConfig[key];
-    });
+    // Object.keys(envConfig).forEach((key) => {
+    //     process.env[key] = envConfig[key];
+    // });
+
+    process.env.JWT_SECRET = 'secret123!';
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'localhost:27017';
 }
